8e6bf0bdb0440607af44b31b7d54b43341e221b6
Mauricio Monroy <mauricio.monroy-j@mail.escuelaing.edu.co>
2024-05-22 12:07:45 -0500

    add_visual_search

--- src/main/java/Mavreactors/app/Controller/LoginController.java
+++ src/main/java/Mavreactors/app/Controller/LoginController.java
@@ -8,12 +8,15 @@ import Mavreactors.app.Model.User;
 import Mavreactors.app.Repository.SessionRepository;
 import Mavreactors.app.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import jakarta.servlet.http.HttpServletResponse;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
@@ -26,6 +29,8 @@ import java.util.UUID;
 public class LoginController {
     private final SessionRepository sessionRepository;
     private final UserRepository userRepository;
+    @Value("${is.prod}")
+    private String isProd;
 
     @Autowired
     public LoginController(SessionRepository sessionRepository, UserRepository userRepository) {
@@ -34,19 +39,42 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> loginSubmit(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<LoginResponse> loginSubmit(
+            @RequestBody LoginRequest loginRequest,
+            HttpServletResponse response) {
         User user = userRepository.findByEmail(loginRequest.getEmail());
         System.out.println("user" + user);
         if (user == null) {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-        } else if (!user.isEnabled()) {
-                return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
         } else if (!user.getPassword().equals(encryptPassword(loginRequest.getPassword()))) {
             return new ResponseEntity<>(null, HttpStatus.FORBIDDEN);
         } else {
             UUID token = UUID.randomUUID();
             Session session = new Session(token, Instant.now(), user);
             sessionRepository.save(session);
+            // Poner en la respuesta, la cookie que deseo que el browser ponga en mi dominio de backend
+            ResponseCookie springCookie = null;
+            if (isProd.equals("true")) {
+                springCookie = ResponseCookie.from("authToken", token.toString())
+                        .path("/")
+                        .httpOnly(true)
+                        .sameSite("None")
+                        .secure(true)
+                        //.domain("wardrobewhizapi.azurewebsites.net")
+                        .build();
+            } else {
+                springCookie = ResponseCookie.from("authToken", token.toString())
+                        .path("/")
+                        .httpOnly(true)
+                        .sameSite("Strict")
+                        .secure(false)
+                        .domain("localhost")
+                        .build();
+            }
+            System.out.println("springCookie: " + springCookie);
+
+            response.addHeader("Set-Cookie", springCookie.toString());
+
             return new ResponseEntity<>(new LoginResponse(user, token), HttpStatus.OK);
         }
     }
